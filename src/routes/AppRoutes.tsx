8b2c93f6c1f12dfb42c86f44b9a833f78e0f486f
thdwldmszz <tiffany_song@naver.com>
2025-11-09 18:36:14 +0900

    feat: My Profile

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -10,6 +10,7 @@ import PurchasingPage from "../purchasing/PurchasingPage";
 import HumanPage from "../human/HumanPage";
 import DashboardPage from "../dashboard/DashboardPage";
 import RequireAuth from "./RequireAuth";
+import UserProfilePage from "../user/UserProfilePage";
 
 import Login from "../auth/pages/Login";
 import AuthCallback from "../auth/pages/AuthCallback";
@@ -32,6 +33,7 @@ const AppRoutes = () => {
         <Route path="/outbound" element={<OutboundPage />} />
         <Route path="/purchasing" element={<PurchasingPage />} />
         <Route path="/human" element={<HumanPage />} />
+        <Route path="/profile" element={<UserProfilePage />} />
       </Route>
 
       <Route path="*" element={<Navigate to="/" replace />} />
