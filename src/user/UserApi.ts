91f10d01e65f84af7b26ea7fec897850a2b74ebd
P-WJ <pwj1103@gmail.com>
2025-11-10 10:05:49 +0900

    fix: api 주소 수정

--- src/user/UserApi.ts
+++ src/user/UserApi.ts
@@ -1,10 +1,9 @@
+import { AUTH_ENDPOINTS, USER_BASE_PATH } from "../api";
 import { fetchWithAuth } from "../auth/api/fetchWithAuth";
 import type { UserInfo } from "./UserTypes";
 
-const BASE_URL = "http://34.120.215.23/user/api/v1";
-
 export async function getUser(userId: number): Promise<UserInfo | null> {
-  const res = await fetchWithAuth(`${BASE_URL}/getUser?userId=${userId}`);
+  const res = await fetchWithAuth(`${USER_BASE_PATH}/getUser?userId=${userId}`);
   if (!res.ok) {
     console.error("사용자 조회 실패");
     return null;
@@ -19,19 +18,16 @@ export async function changePassword(
   newPassword: string,
   confirmPassword: string
 ): Promise<boolean> {
-  const res = await fetchWithAuth(
-    "http://34.120.215.23/api/v1/auth/change-password",
-    {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        userId,
-        currentPassword,
-        newPassword,
-        confirmPassword,
-      }),
-    }
-  );
+  const res = await fetchWithAuth(`${AUTH_ENDPOINTS.CHANGE_PASSWORD}`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      userId,
+      currentPassword,
+      newPassword,
+      confirmPassword,
+    }),
+  });
   const data = await res.json();
   if (data.success) {
     alert("비밀번호가 변경되었습니다.");
